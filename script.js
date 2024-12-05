ea7a4f2c24d993e83489f5808677b2c1ab0178fa
Tharoon123 <64660264+Tharoon123@users.noreply.github.com>
2024-12-05 13:42:23 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri('http://192.168.56.1:8080'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('http://192.168.56.1:8080'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('http://192.168.56.1:8080')
+]).then(start)
+
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+
+    // Image Height and Width Customized
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+      
+      if(result.toString()=="Thor (0.28)"){
+        console.log(result.toString())
+      }
+      
+    })
+  })
+}
+
+function loadLabeledImages() {
+  const labels = ['Sumedha Niroshan', 'Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`http://192.168.56.1:8081/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
